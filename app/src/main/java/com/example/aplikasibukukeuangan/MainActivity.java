e0f12a8a0e251570efdcab8d13becfa5a8b0d192
malfazakki <malfazakki@gmail.com>
2022-10-26 12:50:10 +0700

    Muhammad Al Fatih Muzakki

---
+++ app/src/main/java/com/example/aplikasibukukeuangan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplikasibukukeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
